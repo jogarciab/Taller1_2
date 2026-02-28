dafbdbbe153e8fd6e0dfdbb5ee7f449a635456a7
José Andres Garcia <jogarciab07@gmail.com>
2026-02-27 19:36:01 -0500

    Changes of Frontend

--- Taller1_2/Taller1_2.Frontend/Program.cs
+++ Taller1_2/Taller1_2.Frontend/Program.cs
@@ -1,15 +1,19 @@
 ﻿using Taller1_2.Backend;
 
-var circle = new Circle(nameof(Circle), 5);
-var square = new Square(nameof(Square), 10);
-var rhombous = new Rhombous(nameof(Rhombous), 5, 7, 10);
-var kite = new Kite(nameof(Kite), 7, 6, 5, 8);
-var rectangle = new Rectangle(nameof(Rectangle), 4.568, 67.790);
-var parallelogram = new Parallelogram(nameof(Parallelogram), 14.65, 54.67, 23.09);
-var triangle = new Triangle(nameof(Triangle), 45.56, 12.34, 27.09, 15);
-var trapeze = new Trapeze(nameof(Trapeze), 10, 20, 30, 40, 20);
+var circle = new Circle(name : nameof(Circle), 5);
+var square = new Square(name : nameof(Square), 10);
+var rhombous = new Rhombous(name : nameof(Rhombous), 5, 7, 10);
+var kite = new Kite(name : nameof(Kite), 7, 6, 5, 8);
+var rectangle = new Rectangle(name : nameof(Rectangle), 4.568, 67.790);
+var parallelogram = new Parallelogram(name : nameof(Parallelogram), 14.65, 54.67, 23.09);
+var triangle = new Triangle(name : nameof(Triangle), 45.56, 12.34, 27.09, 15);
+var trapeze = new Trapeze(name : nameof(Trapeze), 10, 20, 30, 40, 20);
 
-var figures = new List<GeometricFigure>() { circle, square, rhombous, kite, rectangle, parallelogram, triangle, trapeze };
+var figures = new List<GeometricFigure> 
+{ 
+    circle, square, rhombous, kite, rectangle, parallelogram, triangle, trapeze 
+};
+
 foreach (var figure in figures)
 {
        Console.WriteLine(figure);
